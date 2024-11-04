b055bf395076e30508763482d13836a1be8642b5
Naveen3251 <naveensakthivel3251@gmail.com>
2024-11-04 11:02:47 +0530

    math-func

---
+++ Functions/inbuilt-functions/math.js
@@ -0,0 +1,5 @@
+console.log(Math.round(4.6)); // Prints: 5
+console.log(Math.ceil(4.1));  // Prints: 5
+console.log(Math.floor(4.9)); // Prints: 4
+console.log(Math.max(1, 3, 2)); // Prints: 3
+console.log(Math.random());  // Prints: a random number between 0 and 1
